618e8dbb5b696e40fcf865e9af4c6d91388cd576
Anirudh Kumar <tintinfana1@gmail.com>
2020-06-02 03:30:02 +0000

    added random target dot generation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null; // need singleton for access from other classes
+    public float curTargetDegree;
+    public GameObject targetDot;
+    public int totalSuccess;
+    public int hitTime;
+    private float timer;
+    
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null){
+            instance = this;
+        } else if (instance != this){
+            Destroy(gameObject);
+        }
+        GenerateTargetPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GenerateTargetPosition()
+    {
+        targetDot.SetActive(false);
+        float angle;
+        angle = 180*Random.value;
+        if (Random.value < 0.5){
+            angle *= -1;
+        }
+        targetDot.transform.rotation = Quaternion.Euler(0,0,angle);
+        targetDot.SetActive(true);
+    }
+
+}
